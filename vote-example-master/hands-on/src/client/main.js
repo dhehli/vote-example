a3d4da94f0687b8958cbb8f897bda2c3f7e9c840
dhehli <dh@netlive.ch>
2016-11-28 17:01:48 +0100

    add new components

--- vote-example-master/hands-on/src/client/main.js
+++ vote-example-master/hands-on/src/client/main.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import VotingComponent from './components/VotingComponent'
-import Layout from './components/Layout'
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import Layout from './components/Layout';
+import VoteController from './components/VoteController';
 
-const myProps = {
-  title: "Javascript",
-  count: 123,
-  percent: 33
-}
+// Sample data
+const allVotes = [
+  {
+    id:          'vote_1',
+    title:       'How is your day?',
+    description: 'Tell me: how has your day been so far?',
+    choices:     [
+      {id: 'choice_1', title: 'Good', count: 7},
+      {id: 'choice_2', title: 'Bad', count: 12},
+      {id: 'choice_3', title: 'Not sure yet', count: 1}
+    ]
+  },
+  {
+    id:          'vote_2',
+    title:       'Programming languages',
+    description: 'What is your preferred language?',
+    choices:     [
+      {id: 'choice_1', title: 'JavaScript', count: 5},
+      {id: 'choice_2', title: 'Java', count: 9},
+      {id: 'choice_3', title: 'Plain english', count: 17}
+    ]
+  }
+];
 
-const vote = {
-  title: 'How is your Day?',
-  description: "Tell me how was your das so far?",
-  choices: [
-    {id: 'chocie_1', title: 'Good', count: 33},
-    {id: 'chocie_2', title: 'Bad', count: 12},
-    {id: 'chocie_3', title: 'Not Sure', count: 1}
-  ]
-}
+const mainComponent = <VoteController allVotes={allVotes}/>;
 
+// Render application
 ReactDOM.render(
-  <Layout>
-    <VotingComponent vote={vote} />
-  </Layout>,
+  <Layout>{mainComponent}</Layout>,
   document.getElementById('voteAppMountPoint')
-)
+);
