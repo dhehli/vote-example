9f1727cbc95c7ca21b924a569d928a882b6ca150
dhehli <dh@netlive.ch>
2016-12-02 18:17:00 +0100

    add Routes & Login

--- vote-example-master/hands-on/src/client/main.js
+++ vote-example-master/hands-on/src/client/main.js
@@ -3,13 +3,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layout from './components/Layout';
+
 import VoteController from './components/VoteController';
+import SingleVoteController from './components/SingleVoteController';
+import VoteComposerController from './components/VoteComposerController';
+import LoginController from './components/LoginController';
+import NoMatch from './components/NoMatch';
 
+import { Router, Route, Redirect } from 'react-router';
 
-const mainComponent = <VoteController />;
+import createHistory from 'history/lib/createHashHistory';
 
-// Render application
-ReactDOM.render(
-  <Layout>{mainComponent}</Layout>,
-  document.getElementById('voteAppMountPoint')
-);
+//https://rackt.github.io/history/stable/HashHistoryCaveats.html
+const history = createHistory({queryKey: false});
+
+const router = <Router history={history}>
+  <Redirect from="/" to="/votes" />
+  <Route path="/" component={Layout}>
+    <Route path="votes" component={VoteController}/>
+    <Route path="votes/:id" component={SingleVoteController} />
+    <Route path="login(/:redirect)" component={LoginController}/>
+    <Route path="compose" component={VoteComposerController} onEnter={LoginController.requireAuth} />
+    <Route path="*" component={NoMatch}/>
+  </Route>
+</Router>;
+
+const mount = document.getElementById('voteAppMountPoint');
+ReactDOM.render(router, mount);
